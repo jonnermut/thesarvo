681bc96e834a2d2df484ce50b603dc255dbb53a0
Jon Nermut <jon.nermut@asdeqlabs.com>
2018-12-16 17:43:53 +1100

    Add new ApiKeys class

---
+++ thesarvoguide2/src/com/thesarvo/guide/client/ApiKeys.java
@@ -0,0 +1,11 @@
+package com.thesarvo.guide.client;
+
+public abstract class ApiKeys
+{
+	/**
+	 * Left deliberately not compiling, so you remember to put the key in.
+	 * Don't commit it !!!!!
+	 */
+	// See Google cloud credentials, for instance https://console.cloud.google.com/apis/credentials?organizationId=0&project=my-project-1498110029748
+	public static final String MAPS_API_KEY=;
+}
